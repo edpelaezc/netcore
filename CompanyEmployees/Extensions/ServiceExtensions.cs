d0fba3d6fe0c15ddb0eacb782ce93647d17451ae
Eduardo Peláez <eapelaezc@correo.url.edu.gt>
2022-04-16 20:56:38 -0600

    chapter 1 - project configuration

---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace CompanyEmployees.Extensions
+{
+	public static class ServiceExtensions
+	{
+		// CORS cross-origin resource sharing
+
+		// AllowAnyOrigin -> WithOrigins("example.com")
+		// AllowAnyMethod -> WithMethods("POST", "GET")
+		// AllowAnyHeader -> WithHeaders("accept", "content-type")
+
+		public static void ConfigureCors(this IServiceCollection services) =>
+			services.AddCors(options => {
+				options.AddPolicy("CorsPolicy", builder =>
+					builder.AllowAnyOrigin()
+					.AllowAnyMethod()
+					.AllowAnyHeader());
+			});
+
+		public static void ConfigureIISIntegration(this IServiceCollection services) =>
+			services.Configure<IISOptions>(options => { });
+	}
+}
---
+++ CompanyEmployees/Program.cs
@@ -0,0 +1,38 @@
+﻿using CompanyEmployees.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+if (app.Environment.IsDevelopment())
+
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
+
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
